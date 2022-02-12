05878921477cf12fa3531db3e1ad6c51ed2e7fb8
Dany <deeelorxxd@gmail.com>
2022-02-12 23:12:22 +0200

    app logic done

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+const orderList = document.querySelector('.oredereList')
+
+document.querySelector('.submit').addEventListener('click', function () {
+  let a = document.querySelector('.input').value;
+  console.log(a);
+  document.querySelector('li').textContent = a;
+
+  
+    const newListElement = document.createElement('li');
+    newListElement.classList.add('li-item');
+    newListElement.appendChild(document.createTextNode(a));
+    orderList.appendChild(newListElement);
+
+});
+
+
+
+
+// TODO: make styling and comment what everything is for
+// * P
+// ? 
