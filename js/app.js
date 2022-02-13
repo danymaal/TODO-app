60d3c24f0d877c0046d4428cfc9774cbd8016a77
Dany <deeelorxxd@gmail.com>
2022-02-13 17:04:48 +0200

    styling done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 'use strict';
 
+const orderList = document.querySelector('.oredereList');
+const clear = document.querySelector('.clear');
 
-const orderList = document.querySelector('.oredereList')
+const listOfElements = document.querySelector('.orderedList222');
 
 document.querySelector('.submit').addEventListener('click', function () {
   let a = document.querySelector('.input').value;
   console.log(a);
   document.querySelector('li').textContent = a;
+  const newListElement = document.createElement('li');
+  newListElement.classList.add('li-item');
+  newListElement.appendChild(document.createTextNode(a));
+  orderList.appendChild(newListElement);
 
-  
-    const newListElement = document.createElement('li');
-    newListElement.classList.add('li-item');
-    newListElement.appendChild(document.createTextNode(a));
-    orderList.appendChild(newListElement);
 
 });
+
+const listItem = document.querySelector('li-item')
+
+
+document.querySelector('.clear').addEventListener('click', function () {
+    console.log('Clear all clicked');
+    listItem.remove()
+}); 
+
+
+
 
