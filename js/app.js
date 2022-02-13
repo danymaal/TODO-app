7df6d6b1e323ed4c3e40e38852c1e820b406573e
Dany <deeelorxxd@gmail.com>
2022-02-13 17:33:41 +0200

    logic done: clear button

--- js/app.js
+++ js/app.js
@@ -5,26 +5,43 @@ const clear = document.querySelector('.clear');
 
 const listOfElements = document.querySelector('.orderedList222');
 
-document.querySelector('.submit').addEventListener('click', function () {
-  let a = document.querySelector('.input').value;
-  console.log(a);
-  document.querySelector('li').textContent = a;
+// document.querySelector('.submit').addEventListener('click', function () {
+//   let a = document.querySelector('.input').value;
+//   console.log(a);
+//   document.querySelector('li').textContent = a;
+//   const newListElement = document.createElement('li');
+//   newListElement.classList.add('li-item');
+//   newListElement.appendChild(document.createTextNode(a));
+//   orderList.appendChild(newListElement);
+// });
+
+const listItem = document.querySelector('li-item');
+
+document.querySelector('.clear').addEventListener('click', function () {
+  console.log('Clear all clicked');
+});
+
+const mainForm = document.querySelector('.main-form');
+
+mainForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log('Clicked in form');
+
+  let input = document.querySelector('.input').value;
+  console.log(input);
+
+  document.querySelector('li').textContent = input;
   const newListElement = document.createElement('li');
   newListElement.classList.add('li-item');
-  newListElement.appendChild(document.createTextNode(a));
+  newListElement.appendChild(document.createTextNode(input));
   orderList.appendChild(newListElement);
 
 
+  
+  const clearButton  =document.querySelector('.clear')
+
+    clearButton.addEventListener('click', function () {
+        newListElement.remove()
+    })
+
 });
-
-const listItem = document.querySelector('li-item')
-
-
-document.querySelector('.clear').addEventListener('click', function () {
-    console.log('Clear all clicked');
-    listItem.remove()
-}); 
-
-
-
-
